50db0760224ebf8a636be7b5867542f74e971119
nippinichi <javier.lopez.sola@gmail.com>
2025-02-08 21:41:11 +0100

    loading from api

--- src/app/hero.interface.ts
+++ src/app/hero.interface.ts
@@ -1,4 +1,9 @@
 export interface Hero {
     id: number;
     name: string;
-}
+    description: string;
+    thumbnail: {
+      path: string;
+      extension: string;
+    };
+  }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,56 +2,15 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero.interface';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of, tap } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  private url = 'http://localhost:3000';
-
-  private heroes: Hero[] = [
-    {
-      id: 1,
-      name: "Batman"
-    }, 
-    {
-      id: 2,
-      name: "Capi"
-    }, 
-    {
-      id: 3,
-      name: "SuperPablo"
-    },
-     {
-      id: 4,
-      name: "MegaJorge"
-    }, 
-    {
-      id: 5,
-      name: "CamiloMan"
-    }, 
-    {
-      id: 6,
-      name: "Timurnator"
-    }, 
-    {
-      id: 7,
-      name: "Green Arrow"
-    }, 
-    {
-      id: 8,
-      name: "Blue Spoon"
-    }, 
-    {
-      id: 9,
-      name: "Red richars"
-    }, 
-    {
-      id: 10,
-      name: "Errejón"
-    },
-  ];
+  private url = 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=dc0d5511d6e318116dbaf0799351db12&hash=0b3952f07ee28b2ea83a39decef4cbac'
+  private heroes: Hero[] = [];
 
   constructor(
     private http: HttpClient
@@ -59,12 +18,20 @@ export class HeroService {
 
 
   public getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(`${this.url}/heroes`);
-
+    return this.http.get<{ data: { results: Hero[] } }>(`${this.url}`).pipe(
+      map(response => response.data.results)
+    );
   }
 
   public getHero(id: number): Observable<Hero> {
-    return this.http.get<Hero>(`${this.url}/heroes/${id}`);
+    console.log(`${this.url}&id=${id}`);
+  
+    return this.http.get<{ data: { results: Hero[] } }>(`${this.url}&id=${id}`).pipe(
+      map(response => {
+        console.log('Response:', response);
+        return response.data.results[0]; 
+      })
+    );
   }
 
   searchHeroes(term: string): Observable<Hero[]> {
